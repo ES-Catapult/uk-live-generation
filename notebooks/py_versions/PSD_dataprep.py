d68a159a9fd87a107e7d9f3260c31c5b8bfbf8eb
jsteinemann <jessica.steinemann@es.catapult.org.uk>
2023-05-11 17:20:10 +0100

    Updated location of BMUFuelType.xls to use relative reference

--- notebooks/py_versions/PSD_dataprep.py
+++ notebooks/py_versions/PSD_dataprep.py
@@ -46,7 +46,9 @@ fuel_types_psd = "https://raw.githubusercontent.com/OSUKED/Power-Station-Diction
 fuel_types_elexon = "https://www.bmreports.com/bmrs/cloud_doc/BMUFuelType.xls"
 
 df_ids = pd.read_csv(url_ids, usecols=["dictionary_id", "sett_bmu_id", "ngc_bmu_id"])
-df_ids = df_ids[df_ids["ngc_bmu_id"].notna()]  # Drop any older power stations that don't have a Settlement BMU ID
+df_ids = df_ids[
+    df_ids["ngc_bmu_id"].notna()
+]  # Drop any older power stations that don't have a Settlement BMU ID
 df_locations = pd.read_csv(url_locations)
 df_common_names = pd.read_csv(url_common_names)
 df_fuel_types_psd = pd.read_csv(fuel_types_psd)
@@ -55,12 +57,14 @@ df_fuel_types_psd = pd.read_csv(fuel_types_psd)
 resp = requests.get(fuel_types_elexon)
 with open(os.path.join(location, "BMUFuelType.xls"), "wb") as output:
     output.write(resp.content)
-df_fuel_types_elexon = pd.read_excel("../data/BMUFuelType.xls")
+df_fuel_types_elexon = pd.read_excel(os.path.join(location, "BMUFuelType.xls"))
 
 # %%
 # Split the IDs dataset so that each of the Settlement BMU IDs becomes its own row
 df_ngc_ids_long = (
-    pd.DataFrame(df_ids["ngc_bmu_id"].str.split(",").tolist(), index=df_ids["dictionary_id"])
+    pd.DataFrame(
+        df_ids["ngc_bmu_id"].str.split(",").tolist(), index=df_ids["dictionary_id"]
+    )
     .stack()
     .reset_index()
     .drop(columns="level_1")
@@ -69,35 +73,53 @@ df_ngc_ids_long = (
 df_ngc_ids_long["ngc_bmuID"] = df_ngc_ids_long["ngc_bmuID"].str.strip()
 
 df_sett_ids_long = (
-    pd.DataFrame(df_ids["sett_bmu_id"].str.split(",").tolist(), index=df_ids["dictionary_id"])
+    pd.DataFrame(
+        df_ids["sett_bmu_id"].str.split(",").tolist(), index=df_ids["dictionary_id"]
+    )
     .stack()
     .reset_index()
     .drop(columns="level_1")
     .rename(columns={0: "sett_bmuID"})
 )
 df_sett_ids_long["sett_bmuID"] = df_sett_ids_long["sett_bmuID"].str.strip()
-df_sett_ids_long["sett_ngc_bmu_matching_ID"] = df_sett_ids_long["sett_bmuID"].str.slice(start=2)
+df_sett_ids_long["sett_ngc_bmu_matching_ID"] = df_sett_ids_long["sett_bmuID"].str.slice(
+    start=2
+)
 
 # %%
 # Merge the exploded IDs dataset with the common names, locations and psd fuel types
-df_psd_merged = df_sett_ids_long.merge(df_common_names, how="left", on="dictionary_id").merge(
-    df_locations, how="left", on="dictionary_id"
-)
+df_psd_merged = df_sett_ids_long.merge(
+    df_common_names, how="left", on="dictionary_id"
+).merge(df_locations, how="left", on="dictionary_id")
 df_psd_merged = df_psd_merged.merge(
-    df_fuel_types_psd, how="left", left_on="sett_ngc_bmu_matching_ID", right_on="ngc_bmu_id"
+    df_fuel_types_psd,
+    how="left",
+    left_on="sett_ngc_bmu_matching_ID",
+    right_on="ngc_bmu_id",
 )
 
 # Merge the fuel types based on the BMU ID and the SETT_BMU_ID
 df_psd_merged = df_psd_merged.merge(
-    df_fuel_types_elexon[["SETT_BMU_ID", "FUEL TYPE"]], how="left", left_on="sett_bmuID", right_on="SETT_BMU_ID"
+    df_fuel_types_elexon[["SETT_BMU_ID", "FUEL TYPE"]],
+    how="left",
+    left_on="sett_bmuID",
+    right_on="SETT_BMU_ID",
 )
 
 # Set the final fuel type from the two datasets
 df_psd_merged["fuel"] = np.where(
-    df_psd_merged["FUEL TYPE"].isnull(), df_psd_merged["fuel_type"], df_psd_merged["FUEL TYPE"]
+    df_psd_merged["FUEL TYPE"].isnull(),
+    df_psd_merged["fuel_type"],
+    df_psd_merged["FUEL TYPE"],
 )
 df_psd_merged = df_psd_merged.drop(
-    columns=["sett_ngc_bmu_matching_ID", "fuel_type", "comments", "SETT_BMU_ID", "FUEL TYPE"]
+    columns=[
+        "sett_ngc_bmu_matching_ID",
+        "fuel_type",
+        "comments",
+        "SETT_BMU_ID",
+        "FUEL TYPE",
+    ]
 )
 
 # %%
